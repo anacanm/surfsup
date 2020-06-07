08596535f33b4c381e2bd3b278cd22ec9008e39a
anacanm <anacan@mangelsdorf.io>
2020-06-07 09:48:37 -0700

    Add test for Parser.inser_keys_into_row_data/1

--- buoy_collection/test/parser_test.exs
+++ buoy_collection/test/parser_test.exs
@@ -10,4 +10,17 @@ defmodule ParserTest do
 
     assert Parser.clean_row_data(sample_row_data) == expected_result
   end
+
+  test "insert_keys_into_row_data" do
+    sample_row_data = ["2020", "06", "07", "15", "26", "1.5", "0.5", "15.4", "1.4", "7.1", "SSW",
+    "WSW", "STEEP", "6.4", "256"]
+    
+    expected_result = ["year", "2020", "month", "06", "day", "07", "hour", "15", "minute", "26",
+    "wave_height", "1.5", "swell_height", "0.5", "swell_period", "15.4",
+    "wind_wave_height", "1.4", "wind_wave_period", "7.1", "swell_direction", "SSW",
+    "wind_wave_direction", "WSW", "steepness", "STEEP", "average_wave_period",
+    "6.4", "mean_wave_direction", "256"]
+
+    assert Parser.inject_keys_into_row_data(sample_row_data) == expected_result
+  end
 end
