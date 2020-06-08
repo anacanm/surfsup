3cfcdedacb15fd7ce83f497631ad8446203c424e
anacanm <anacan@mangelsdorf.io>
2020-06-07 17:43:00 -0700

    Add parse_numbers to Parser module, make helper functions private

--- buoy_collection/lib/buoy_collection/parser.ex
+++ buoy_collection/lib/buoy_collection/parser.ex
@@ -10,37 +10,38 @@ defmodule BuoyCollection.Parser do
     |> clean_row_data()
     |> inject_keys_into_row_data()
     |> keys_and_data_to_struct()
+    |> parse_numbers()
     |> convert_meters_to_feet()
   end
 
-  # ! make all functions below this line private
+  ####################################################
 
   @spec clean_row_data(String.t()) :: list
-  def clean_row_data(row_data) do
+  defp clean_row_data(row_data) do
     row_data
     |> String.split(" ")
     |> Enum.filter(fn elem -> elem != "" end)
   end
 
   @spec inject_keys_into_row_data(list) :: list
-  def inject_keys_into_row_data(row_data) do
+  defp inject_keys_into_row_data(row_data) do
     List.zip([atom_keys(), row_data])
   end
 
   @spec keys_and_data_to_struct(list) :: %BuoyData{}
-  def keys_and_data_to_struct(buoy_data_with_keys) do
+  defp keys_and_data_to_struct(buoy_data_with_keys) do
     struct(BuoyCollection.BuoyData, buoy_data_with_keys)
   end
 
   @spec convert_meters_to_feet(%BuoyData{}) :: %BuoyData{}
-  def convert_meters_to_feet(buoy_data = %BuoyData{wave_height: wh, swell_height: sh, wind_wave_height: wwh}) do
+  defp convert_meters_to_feet(buoy_data = %BuoyData{wave_height: wh, swell_height: sh, wind_wave_height: wwh}) do
     %BuoyData{buoy_data | wave_height: to_feet(wh), swell_height: to_feet(sh), wind_wave_height: to_feet(wwh)}
   end
 
   @spec to_feet(number) :: float
-  def to_feet(value), do: Float.round(value * 3.28084, 3)
+  defp to_feet(value), do: Float.round(value * 3.28084, 3)
 
-  def atom_keys do
+  defp atom_keys do
     [
       :year,
       :month,
@@ -60,8 +61,70 @@ defmodule BuoyCollection.Parser do
     ]
   end
 
+  ####################################################
+  # parse helper functions
+
+  @spec parse_numbers(%BuoyData{}) :: %BuoyData{}
+  defp parse_numbers(buoy_data = %BuoyData{}) do
+    buoy_data
+    |> parse_integers()
+    |> parse_floats()
+  end
+
+  defp parse_integers(buoy_data = %BuoyData{year: year, month: month, day: day, hour: hour, minute: minute}) do
+    %BuoyData{
+      buoy_data
+      | year: parse(year),
+        month: parse(month),
+        day: parse(day),
+        hour: parse(hour),
+        minute: Integer.parse(minute)
+    }
+  end
+
+  defp parse_floats(
+         buoy_data = %BuoyData{
+           average_wave_period: awp,
+           mean_wave_direction: mwd,
+           swell_height: sh,
+           swell_period: sp,
+           wave_height: wh,
+           wind_wave_height: wwh,
+           wind_wave_period: wwp
+         }
+       ) do
+    %BuoyData{
+      buoy_data
+      | average_wave_period: parse(awp),
+        mean_wave_direction: parse(mwd),
+        swell_height: parse(sh),
+        swell_period: parse(sp),
+        wave_height: parse(wh),
+        wind_wave_height: parse(wwh),
+        wind_wave_period: parse(wwp)
+    }
+  end
+
+  defp parse(value) do
+    handle_parse(value, String.contains?(value, "."))
+  end
+
+  defp handle_parse(num, _is_float = true) do
+    case Float.parse(num) do
+      {val, _} -> val
+      :error -> raise ArgumentError, message: "#{num} cannot be parsed to a float"
+    end
+  end
+
+  defp handle_parse(num, _is_float = false) do
+    case Integer.parse(num) do
+      {val, _} -> val
+      :error -> raise ArgumentError, message: "#{num} cannot be parsed to an integer"
+    end
+  end
+
   # no longer being used, but saved for potential further use
-  # def keys do
+  # defp keys do
   #   [
   #     "year",
   #     "month",
--- buoy_collection/test/parser_test.exs
+++ buoy_collection/test/parser_test.exs
@@ -3,35 +3,36 @@ defmodule ParserTest do
   alias BuoyCollection.Parser
   doctest Parser
 
-  test "clean_row_data returns list with removed spaces" do
-    sample_row_data = "2020 06 07 04 26  1.8  0.8 14.3  1.6  6.2 SSW   W VERY_STEEP  5.7 270"
-    expected_result = ["2020", "06", "07", "04", "26", "1.8", "0.8", "14.3", "1.6", "6.2", "SSW", "W",
-    "VERY_STEEP", "5.7", "270"]
-
-    assert Parser.clean_row_data(sample_row_data) == expected_result
-  end
-
-  test "insert_keys_into_row_data" do
-    sample_row_data = ["2020", "06", "07", "15", "26", "1.5", "0.5", "15.4", "1.4", "7.1", "SSW",
-    "WSW", "STEEP", "6.4", "256"]
-
-    expected_result = [
-      year: "2020",
-      month: "06",
-      day: "07",
-      hour: "15",
-      minute: "26",
-      wave_height: "1.5",
-      swell_height: "0.5",
-      swell_period: "15.4",
-      wind_wave_height: "1.4",
-      wind_wave_period: "7.1",
-      swell_direction: "SSW",
-      wind_wave_direction: "WSW",
-      steepness: "STEEP",
-      average_wave_period: "6.4",
-      mean_wave_direction: "256"
-    ]
-    assert Parser.inject_keys_into_row_data(sample_row_data) == expected_result
-  end
+  # below functions are no longer public
+  #  test "clean_row_data returns list with removed spaces" do
+  #    sample_row_data = "2020 06 07 04 26  1.8  0.8 14.3  1.6  6.2 SSW   W VERY_STEEP  5.7 270"
+  #    expected_result = ["2020", "06", "07", "04", "26", "1.8", "0.8", "14.3", "1.6", "6.2", "SSW", "W",
+  #    "VERY_STEEP", "5.7", "270"]
+  #
+  #    assert Parser.clean_row_data(sample_row_data) == expected_result
+  #  end
+  #
+  #  test "insert_keys_into_row_data" do
+  #    sample_row_data = ["2020", "06", "07", "15", "26", "1.5", "0.5", "15.4", "1.4", "7.1", "SSW",
+  #    "WSW", "STEEP", "6.4", "256"]
+  #
+  #    expected_result = [
+  #      year: "2020",
+  #      month: "06",
+  #      day: "07",
+  #      hour: "15",
+  #      minute: "26",
+  #      wave_height: "1.5",
+  #      swell_height: "0.5",
+  #      swell_period: "15.4",
+  #      wind_wave_height: "1.4",
+  #      wind_wave_period: "7.1",
+  #      swell_direction: "SSW",
+  #      wind_wave_direction: "WSW",
+  #      steepness: "STEEP",
+  #      average_wave_period: "6.4",
+  #      mean_wave_direction: "256"
+  #    ]
+  #    assert Parser.inject_keys_into_row_data(sample_row_data) == expected_result
+  #  end
 end
